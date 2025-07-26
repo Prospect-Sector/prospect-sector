4f2906b5b22ee7e668339ed8652e78b69bda7df0
GreaseMonk <1354802+GreaseMonk@users.noreply.github.com>
2025-07-27 00:57:03 +0200

    Add ending tag

--- Content.Shared/_PS/CCVar/CCVars.PS.cs
+++ Content.Shared/_PS/CCVar/CCVars.PS.cs
@@ -4,6 +4,7 @@ namespace Content.Shared._PS.CCVar;
 
 /// <summary>
 ///     General CCVars for the Prospect Sector game mode.
+/// </summary>
 public sealed partial class CCVars
 {
     // TODO: Add more CCVars as needed for the Prospect Sector game mode.
