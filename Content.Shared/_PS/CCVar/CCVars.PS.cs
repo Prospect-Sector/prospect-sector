49064590bb23c7285a1cb873ad0cdbb5eeef2290
GreaseMonk <1354802+GreaseMonk@users.noreply.github.com>
2025-07-27 00:45:43 +0200

    Base gamemode

---
+++ Content.Shared/_PS/CCVar/CCVars.PS.cs
@@ -0,0 +1,10 @@
+﻿using Robust.Shared.Configuration;
+
+namespace Content.Shared._PS.CCVar;
+
+/// <summary>
+///     General CCVars for the Prospect Sector game mode.
+public sealed partial class CCVars
+{
+    // TODO: Add more CCVars as needed for the Prospect Sector game mode.
+}
